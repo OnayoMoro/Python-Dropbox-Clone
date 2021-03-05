c748797ddde72c98aa7367ee038b0a0db201ae15
OnayoMoro <45406920+OnayoMoro@users.noreply.github.com>
2021-03-05 00:17:24 +0000

    Added delete function
    Added new delete capabilities.
    
    Made everything a little more stable

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+#Client code
+
 import socket
 import time
 from pathlib import Path
@@ -19,11 +21,16 @@ Files = namedtuple('File', 'name path size modified_date')
 files1 = [] #Keeps track of destination dir server side
 files2 = [] #Keeps track of source dir client side
 p = Path(fp)
+delay = .2
 
 
 #Send files
 def sendfiles(files):
     print(files)
+
+    #Send request
+    request = "add"
+    s.send(bytes(request, "utf-8"))
     
     #Send file name and extension
     file_name = files.name
@@ -39,6 +46,66 @@ def sendfiles(files):
     s.send(file_data)
 
 
+def deletefiles(files):
+    print(files)
+
+    #Send request
+    request = "delete"
+    s.send(bytes(request, "utf-8"))
+
+    #Send file name and extension
+    file_name = files.name
+    s.send(bytes(file_name, "utf-8"))
+    print("Deleting file: ", file_name)
+    
+
+
+def addingfiles():
+    for i in range(len(files2)):
+        try:
+            #If file is on server
+            if files2[i].name == files1[i].name:
+                
+                #If file has not been modified
+                if files2[i].modified_date == files1[i].modified_date:
+                    print("File ", files2[i].name, " is up to date")
+                    
+                #If file has been modified
+                else:
+                    files1[i] = files2[i]
+                    sendfiles(files2[i])
+                    
+            #If file is not on server
+            elif files2[i] not in files1:
+                files1.append(files2[i])
+                sendfiles(files2[i])
+        except:
+            #If file is not on server
+            if files2[i] not in files1:
+                files1.append(files2[i])
+                sendfiles(files2[i])
+            else:
+                print("Something went wrong when trying to update files")
+        time.sleep(delay)
+
+
+def deletingfiles():
+    i=0
+    while (i<len(files1)):
+        #If file is deleted on client
+        if files1[i] not in files2:
+            print("File ", files1[i].name, "deleted")
+            deletefiles(files1[i])
+            files1.remove(files1[i])
+            time.sleep(delay)
+                
+        #If file is not deleted
+        else:
+            print("File ", files1[i].name, "not deleted")
+            #time.sleep(delay)
+        i+=1
+
+
 #Loop through files to populate/initialise base file list
 for item in p.glob('**/*'):
     name = item.name
@@ -51,11 +118,14 @@ for item in p.glob('**/*'):
 
     #Send files to server for the first time
     sendfiles(Files(name, path, size, modified))
-    time.sleep(2)
+    time.sleep(delay)
 
 
-#Loop to constantly re-populate new file list 
+#Loop to constantly re-populate new file list
+#and keep the destination dir up to date
 while (1==1):
+
+    length = len(files1)
 
     #Add current files to new file list
     for item in p.glob('**/*'):
@@ -64,39 +134,14 @@ while (1==1):
         size = item.stat().st_size
         modified = datetime.fromtimestamp(item.stat().st_mtime)
 
-
         #Add file to new file list
         files2.append(Files(name, path, size, modified))
 
     #Compare lists for changes for ADDING new files to server
-    for i in range(len(files2)):
-        try:
-            if files2[i] in files1:
-                print("File ", files2[i].name, " is up to date")
-            elif files2[i] not in files1:
-                files1.append(files2[i])
-                sendfiles(files2[i])
-        except:
-            print("Something went wrong")
-        time.sleep(2)
-
+    addingfiles()
+    
     #Compare lists for changes for DELETING new files from server
-
+    deletingfiles()
+        
     #Clear new file list 
     files2 = []
-                
-
-
-    
-    
-
-
-
-    
-    
-
-
-
-
-#If changes detected, send files to server
-
--- server_folder/server.py
+++ server_folder/server.py
@@ -1,4 +1,7 @@
+#Server code
+
 import socket
+import os
 
 s = socket.socket()
 host = socket.gethostname()
@@ -10,16 +13,35 @@ print("Waiting for client request...")
 conn, addr = s.accept()
 print(addr, "has successfully connected to the server")
 
+def addfile():
+    file_name = conn.recv(204800000).decode("utf-8")
+    print("Updating file: ", file_name)
+    file_data = conn.recv(204800000)
+    file = open(file_name, 'wb')
+    file.write(file_data)
+    file.close()
+    print("File recieved")
+
+def deletefile():
+    print("Deletefile method hit")
+    file_name = conn.recv(204800000).decode("utf-8")
+    print("Deleting file: ", file_name)
+    os.remove(file_name)
+    print("File deleted")
+
 while (1==1):
     
     try:
-        file_name = conn.recv(204800000)
-        print("Updating file: ", file_name.decode("utf-8"))
-        file_data = conn.recv(204800000)
-        file = open(file_name.decode("utf-8"), 'wb')
-        file.write(file_data)
-        file.close()
-        print("File recieved")
+        print("Waiting for client request...")
+        request = conn.recv(2048).decode("utf-8")
+        print(request)
+        if (request ==  "add"):
+            addfile()
+        elif(request == "delete"):
+            deletefile()
+        
+        
+        
     except:
         conn, addr = s.accept()
 
